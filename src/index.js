ad0b6d5f564c5dd2c847358e453a4dc4f23850cb
Tomas Bivainis <bivainis.tomas@gmail.com>
2024-06-24 18:19:03 +0300

    you can now add tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
 import './styles.css';
+import { createTask, createProject } from "./module";
+const divTasks = document.querySelector("#tasks");
+const inputProjectTitle = document.querySelector('#project_name');
+const buttonNewTask = document.querySelector('#new_task');
 
-console.log("log");
+let projects = [];
+
+projects.push(createProject());
+
+let selectedProject = 0;
+
+function displayTasks() {
+  divTasks.innerHTML = "";
+
+  projects[selectedProject].tasks.forEach(task => {
+    const divNewTask = document.createElement('div');
+    divNewTask.classList.add('task');
+
+    const title = document.createElement('input');
+    title.value = task.title;
+    title.type = 'text';
+
+    const description = document.createElement('textarea');
+    description.value = task.description;
+
+    const dueDate = document.createElement('input');
+    dueDate.value = task.dueDate;
+    dueDate.type = 'date';
+
+    const priority = document.createElement('input');
+    priority.value = task.priority;
+    priority.type = 'number';
+
+    divNewTask.appendChild(title);
+    divNewTask.appendChild(dueDate);
+    divNewTask.appendChild(priority);
+    divNewTask.appendChild(description);
+
+    divTasks.appendChild(divNewTask);
+  });
+}
+
+function displayProject() {
+  inputProjectTitle.value = projects[selectedProject].title;
+  displayTasks();
+}
+
+function changeProject(newSelectedProject) {
+  selectedProject = newSelectedProject;
+  displayProject();
+}
+
+function changeProjectTitle() {
+  projects[selectedProject].title = inputProjectTitle.value;
+}
+
+function newTask() {
+  projects[selectedProject].tasks.push(createTask());
+  displayTasks();
+}
+
+inputProjectTitle.addEventListener('input', changeProjectTitle);
+buttonNewTask.addEventListener('click', newTask);
+
+displayProject();
+
---
+++ src/module.js
@@ -0,0 +1,33 @@
+function createTask() {
+  return {
+    "title": "Untitled",
+    "description": "",
+    "dueDate": getCurrentDate(),
+    "priority": "1",
+  }
+}
+
+function createProject() {
+  return {
+    "title": "Untitled",
+    "tasks": [],
+  }
+}
+
+function getCurrentDate() {
+  let date = new Date();
+
+  let year = `${date.getFullYear()}`;
+  let month = `${date.getMonth()}`;
+  let day = `${date.getDate()}`;
+
+  if(month.length == 1) month = `0${month}`;
+  if(day.length == 1) day = `0${day}`;
+
+  return `${year}-${month}-${day}`;
+}
+
+module.exports = {
+  createTask,
+  createProject,
+}
